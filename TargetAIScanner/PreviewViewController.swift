217b9c49227a01f7d67545b71c418962c12280dc
Owais Raza <owaisraza64@gmail.com>
2019-08-17 10:34:20 -0500

    Camera working, Save and Cancel buttons working

--- TargetAIScanner/PreviewViewController.swift
+++ TargetAIScanner/PreviewViewController.swift
@@ -9,18 +9,26 @@ import UIKit
 
 class PreviewViewController: UIViewController {
 
+    var image: UIImage!
+    
+    @IBOutlet weak var photo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        photo.image = self.image
 
         // Do any additional setup after loading the view.
     }
     
 
     @IBAction func Cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     
     @IBAction func Save(_ sender: Any) {
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        dismiss(animated: true, completion: nil)
     }
     
     /*
