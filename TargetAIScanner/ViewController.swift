c19916b61bbb909461806ff0b7412cfa04a43592
Owais Raza <owaisraza64@gmail.com>
2019-08-17 08:49:05 -0500

    Initial Commit

---
+++ TargetAIScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TargetAIScanner
+//
+//  Created by Owais Raza on 8/17/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
